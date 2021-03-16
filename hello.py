a4fabdc38f850a67c7c09cc7026e28af718274fb
Asad Jamali <asadjamali15@gmail.com>
2021-03-16 16:53:12 +0500

    added author information

---
+++ hello.py
@@ -0,0 +1,17 @@
+##############
+#learn python in 2021
+#asad ali jamali
+#git asad jamali
+#######################
+
+#clear terminal screen
+import os
+os.system ('clear')
+
+
+
+#this first coding on python
+
+
+
+print ("hello world!")
